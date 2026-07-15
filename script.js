ce9a79bf8687d27ca7da409f9d8a536f127baaca
leth5 <letharqic4@gmail.com>
2026-07-15 21:38:09 +0200

    *

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
+import {MD5} from "./md5.js"
+
 const main = document.getElementById('main');
+const passwordInput = document.getElementById('password')
 
-fetch('https://script.google.com/macros/s/AKfycby9yBvR_sKZSMt1pXjhcgstOQB6m6WtTIjD1IZnjVvwOCTZMSBQipA_u-cY8Qdascg3/exec')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .then(data => create(data));
+const link = 'https://script.google.com/macros/s/AKfycbyg8nGYvoqVCrziF8I1KxFVFyL9q2ALyV2rDgRq5eABW0txyoe0bH_U7iFDfna7MsGVPQ/exec';
 
+passwordInput.addEventListener('input', () => checkPassword())
 
-function create(data) {
-    var element = document.createElement('div');
-    element.innerHTML = '123';
-    document.body.appendChild(element);
+checkPassword();
+
+function getData() {
+    fetch(link)
+    .then(res => res.json())
+    .then(data => create(data));
 }
 
-function call() {
-    fetch('https://script.google.com/macros/s/AKfycbxrUd5dTze3FjQb8NBouYCla2sBdMTgroNsxCxHP4E7C91ZlFhFZ2rT42NQEX0W2mH48A/exec', {
+function create(data) {
+    
+    for (let r = 0; r < data[0].length; r++) {
+        var row = document.createElement('div');
+        row.classList.add('row');
+        row.innerHTML = data[0][r];
+
+        for (let p = 1; p < data.length; p++) {
+            let point = document.createElement('button');
+            point.classList.add('point');
+            let value = data[p][r];
+            point.innerHTML = value;
+            if (value) {
+                point.classList.add('filled');
+            }
+            row.appendChild(point);
+
+            point.addEventListener('click', () => {
+                
+                let newValue = Math.abs(1 - point.innerHTML);
+                call(p, r, newValue)
+                point.innerHTML = newValue;
+
+                if (newValue) {
+                    point.classList.add('filled');
+                } else {
+                    point.classList.remove('filled');
+                }
+            });
+        }
+
+
+        document.body.appendChild(row);
+    }
+    
+}
+
+function checkPassword() {
+    if (MD5(document.getElementById('password').value) == "dd959a1a26196f00da4330a3b3a42b63") {
+        document.getElementById('password').remove();
+        getData();
+    }
+}
+
+function call(row, column, value) {
+    fetch(link, {
         method: 'POST',
         headers: { 'Content-Type': "text/plain;charset=utf-8"},
         redirect: "follow",
-        body: JSON.stringify(['test1'])
+        body: JSON.stringify({row: row + 1, column: column + 1, value: value})
     });
 }
 
